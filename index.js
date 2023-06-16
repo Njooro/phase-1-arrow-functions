bc6383e72b60367150d4096f6ab5af86614640f6
Maxwell <njoromax5@gmail.com>
2023-06-16 10:13:42 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(num1=2000, num2=100){
+return num1/num2
+}
+console.log(divide(2000/100));
+
+const square = (x) => {
+    return x*x
+}
+console.log(2*2)
+
+const add = (a, b) => {
+    return a + b
+}
+console.log(add(3,4))
+
